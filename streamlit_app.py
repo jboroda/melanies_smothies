9b6a20033c13ad829c815b4c1fb3e596d84d7387
jboroda <jboroda@users.noreply.github.com>
2025-06-09 19:32:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,9 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
-#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-#sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # # Get the current credentials
 # session = get_active_session()
